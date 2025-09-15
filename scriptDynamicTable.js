463462ac7fae435b1e4d7626d6cd349e81c7ff41
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2025-09-15 11:27:10 +0100

    refactor

--- scriptDynamicTable.js
+++ scriptDynamicTable.js
@@ -1,41 +1,25 @@
-// passing the list of lists:
-let tableList =
-  [
-    ["col1 header", "col2 header", "col3 header", "col4 header", "col5"],
-    ["data1", "data2", "data3", "data4", "col5"],
-    ["ciao1", "ciao2", "ciao3", "ciao4", "col5"],
-    ["data5", "data6", "data7", "data8", "col5"],
-    ["data9", "data10", "data11", "data12", "col5"],
-    ["ciccio1", "ciccio2", "ciccio3", "ciccio4","col5"],
-    ["ciccio5", "ciccio6", "ciccio7", "a", "col5"]
-  ];
 
+// example on how to pass the data to the data: (the first list is considered the header of the function) 
 let myTable = [
-  ["col1", "col2", "col3", "col4", "col5", "col6"],
-  ["data1", "data2", "data3", "data4", "data5", "data6"],
-  ["data7", "data8", "data9", "data10", "data11", "data12"],
-  ["data13", "data14", "data15", "data16", "data17", "data18"],
-  ["data19", "data20", "data21", "data22", "data23", "data24"],
-]
+        ["col1", "col2", "col3", "col4", "col5", "col6"],
+        ["data1", "data2", "data3", "data4", "data5", "data6"],
+        ["data7", "data8", "data9", "data10", "data11", "data12"],
+        ["data13", "data14", "data15", "data16", "data17", "data18"],
+        ["data19", "data20", "data21", "data22", "data23", "data24"],
+      ]
 
-
+// passing the list of lists:
 function tableGenerator(tableList) {
-  // provide a list of lists. the first list is considered as header
-  let table = "";
+  /* provide a list of lists. the first list is considered as header */
+  let table = `<table style="border-collapse: collapse; width: 90%; font-family: Arial;">\n`;
   let numberColumnsTable = tableList[0].length;
-  
+  let tableCloseTag = `</table>\n`;
   // columns structure check
   for (let i = 1; i < tableList.length; i++) {
     if (tableList[i].length !== numberColumnsTable) {
       return "ERROR: All the rows have to have the same number of columns";
     }
   }
-  
-  // table: opening and closing creation
-  let tableOpenTag = `<table style="border-collapse: collapse; width: 90%; font-family: Arial;">\n`;
-  let tableCloseTag = `</table>\n`;
-  table += tableOpenTag;
-
   // first line table
   let trOpen = '  <tr style="font-size: 14px; background-color: rgb(216, 216, 216);">\n';
   let rowBody = "";
@@ -44,7 +28,6 @@ function tableGenerator(tableList) {
     rowBody += `   <th style="border: 1px solid rgb(172, 172, 172); padding: 7px; text-align: left;">${tableList[0][index]}</th>${index !== numberColumnsTable - 1 ? "\n" : ""}`;
   }
   table += trOpen += rowBody += trClose // adding the first row to the table
-
   // rows with actual data
   for (let row = 1; row <= tableList.length - 1; row++) {
     trClose = "  </tr>\n";
@@ -55,9 +38,8 @@ function tableGenerator(tableList) {
     }
     table += trOpen += rowBody += trClose; // adding the row to the table
   }
-
+  // closing the tab
   table += tableCloseTag;
   return table;
 }
 
-console.log(tableGenerator(tableList));
